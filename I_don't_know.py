59512032a7d7aa538721971ba8a37bc1de2ccbbe
I'm ridhoae303 <ridhooffweb@gmail.com>
2026-06-19 19:56:40 +0700

    I don't know
    Don't nosy!

--- I_don't_know.py
+++ I_don't_know.py
@@ -1,1 +1,1 @@
-# What are you doing here idiot!?!? this is just a dummy file you nosy bastard!
+# What are you doing here, you idiot? These are just dummy files I made on purpose, and you’re nosy about what’s in here, you weirdo!
